98d8f9ec457092010ed494365c0085b95053e92b
LSQo3o <18743172435@163.com>
2023-06-04 15:36:46 +0800

    1

--- A-Questions/冒泡排序.py
+++ A-Questions/冒泡排序.py
@@ -6,3 +6,11 @@
 @Author  ：四七
 @Date    ：2023/5/28 15:27
 """
+lst = [2, 4, 7, 9, 6, 10]
+for i in range(0, len(lst)):
+    for j in range(0, len(lst)-1):
+        if lst[j] > lst[j + 1]:
+            lst[j], lst[j + 1] = lst[j + 1], lst[j]
+print(lst)
+
+
